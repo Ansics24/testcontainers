23ed8097f17b0375bde145bfef7ff59dc38e9a31
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2022-08-25 20:27:45 +0200

    junit4 integration

--- src/test/java/de/schulte/testcontainers/ContainerTest.java
+++ src/test/java/de/schulte/testcontainers/ContainerTest.java
@@ -1,33 +1,33 @@
 package de.schulte.testcontainers;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
-class ContainerTest {
+public class ContainerTest {
 
-    @Container
-    private GenericContainer postgres = new GenericContainer<>(DockerImageName.parse("postgres"));
+    @Rule
+    public GenericContainer postgres = new GenericContainer<>(DockerImageName.parse("postgres"));
 
-    @Container
-    private static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis"));
+    @ClassRule
+    public static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis"));
 
     @Test
-    void firstContainerTest() {
-        Assertions.assertTrue(postgres.isRunning());
-        Assertions.assertTrue(redis.isRunning());
+    public void firstContainerTest() {
+        assertTrue(postgres.isRunning());
+        assertTrue(redis.isRunning());
         System.out.println("Postgres Containername: " + postgres.getContainerName());
         System.out.println("Redis Containername: " + redis.getContainerName());
     }
 
     @Test
-    void secondContainerTest() {
-        Assertions.assertTrue(postgres.isRunning());
-        Assertions.assertTrue(redis.isRunning());
+    public void secondContainerTest() {
+        assertTrue(postgres.isRunning());
+        assertTrue(redis.isRunning());
         System.out.println("Postgres Containername: " + postgres.getContainerName());
         System.out.println("Redis Containername: " + redis.getContainerName());
     }
