1aa6387177d921acbbcb27baf04f154c5fb2277d
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2022-08-25 20:27:57 +0200

    Revert "junit4 integration"
    This reverts commit 23ed8097f17b0375bde145bfef7ff59dc38e9a31.

--- src/test/java/de/schulte/testcontainers/ContainerTest.java
+++ src/test/java/de/schulte/testcontainers/ContainerTest.java
@@ -1,33 +1,33 @@
 package de.schulte.testcontainers;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-public class ContainerTest {
+@Testcontainers
+class ContainerTest {
 
-    @Rule
-    public GenericContainer postgres = new GenericContainer<>(DockerImageName.parse("postgres"));
+    @Container
+    private GenericContainer postgres = new GenericContainer<>(DockerImageName.parse("postgres"));
 
-    @ClassRule
-    public static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis"));
+    @Container
+    private static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis"));
 
     @Test
-    public void firstContainerTest() {
-        assertTrue(postgres.isRunning());
-        assertTrue(redis.isRunning());
+    void firstContainerTest() {
+        Assertions.assertTrue(postgres.isRunning());
+        Assertions.assertTrue(redis.isRunning());
         System.out.println("Postgres Containername: " + postgres.getContainerName());
         System.out.println("Redis Containername: " + redis.getContainerName());
     }
 
     @Test
-    public void secondContainerTest() {
-        assertTrue(postgres.isRunning());
-        assertTrue(redis.isRunning());
+    void secondContainerTest() {
+        Assertions.assertTrue(postgres.isRunning());
+        Assertions.assertTrue(redis.isRunning());
         System.out.println("Postgres Containername: " + postgres.getContainerName());
         System.out.println("Redis Containername: " + redis.getContainerName());
     }
