d886e2510616eb45655445f864538e33d2c6a6e9
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2022-08-26 14:28:18 +0200

    Dependencies

--- src/test/java/de/schulte/testcontainers/TaskListServiceTest.java
+++ src/test/java/de/schulte/testcontainers/TaskListServiceTest.java
@@ -1,11 +1,9 @@
 package de.schulte.testcontainers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -13,8 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.output.ToStringConsumer;
-import org.testcontainers.containers.output.WaitingConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.junit.jupiter.Container;
@@ -25,8 +21,6 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Testcontainers
 class TaskListServiceTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(TaskListServiceTest.class);
 
     @Container
     private GenericContainer postgres =
@@ -40,11 +34,16 @@ class TaskListServiceTest {
                             .withStrategy(Wait.forLogMessage(".*is ready to accept connections.*", 1))
                             .withStrategy(Wait.defaultWaitStrategy()));
 
+    @Container
+    private GenericContainer nginx =
+            new GenericContainer(DockerImageName.parse("nginx:1.23")).withExposedPorts(80).dependsOn(postgres);
+
     private TaskListService serviceUnderTest;
 
     @BeforeEach
     void followContainerLogs() throws TimeoutException {
-        postgres.followOutput(new Slf4jLogConsumer(LOGGER));
+        postgres.followOutput(new Slf4jLogConsumer(LoggerFactory.getLogger("Postgres Logger")));
+        nginx.followOutput(new Slf4jLogConsumer(LoggerFactory.getLogger("Nginx Logger")));
     }
 
     @BeforeEach
